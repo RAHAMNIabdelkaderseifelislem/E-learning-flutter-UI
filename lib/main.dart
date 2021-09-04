cf4550a309c617d5c76abf76e7673b6ea18ce8c3
RAHMANI Abd El Kader Seif El Islem <77025510+RAHAMNIabdelkaderseifelislem@users.noreply.github.com>
2021-09-04 13:05:31 +0100

    ui

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:elearning/ui/pages/onboarding1.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+SharedPreferences prefs;
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences.getInstance().then((prefs) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+        .then((value) => runApp(
+              RestartWidget(
+                child: MyApp(),
+              ),
+            ));
+  });
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  void getLoginStatus() async {
+    prefs = await SharedPreferences.getInstance();
+    
+    prefs.setBool("isLoggedin", true);
+    
+  }
+
+  @override
+  void initState() {
+    getLoginStatus();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      home: Onboarding(),
+    );
+  }
+}
+
+class RestartWidget extends StatefulWidget {
+  RestartWidget({this.child});
+
+  final Widget child;
+
+  static void restartApp(BuildContext context) {
+    context.findAncestorStateOfType<_RestartWidgetState>().restartApp();
+  }
+
+  @override
+  _RestartWidgetState createState() => _RestartWidgetState();
+}
+
+class _RestartWidgetState extends State<RestartWidget> {
+  Key key = UniqueKey();
+
+  void restartApp() {
+    setState(() {
+      key = UniqueKey();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return KeyedSubtree(
+      key: key,
+      child: widget.child,
+    );
+  }
+}
